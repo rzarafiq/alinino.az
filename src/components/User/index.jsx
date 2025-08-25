b6a614523661afb6be353ea8f12debba12db368a
Rza Rafiq <rza.rafiq@mail.ru>
2025-08-25 21:05:01 +0400

    first commit

---
+++ src/components/User/index.jsx
@@ -0,0 +1,15 @@
+import Login from "./Login"
+import RecoverPassword from "./RecoverPassword"
+import Registration from "./Registration"
+
+function User() {
+  return (
+    <>
+        <Login />
+        <Registration />
+        <RecoverPassword />
+    </>
+  )
+}
+
+export default User
